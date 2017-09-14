2f754c8d1abbea70225a74cf7feb29cbbb862398
Alexander Fridman <alexfridman@outlook.com>
2017-09-14 09:33:53 +0300

    Add dropout to run_rgb.sh

--- run_rgb.sh
+++ run_rgb.sh
@@ -3,5 +3,5 @@ python3 main.py ucf101 RGB \
     /media/e/vsd/data/ucf101_preprocessed/split_01/file_lists/test_rgb.txt \
     --arch BNInception --num_segments 3 \
     --gd 20 --lr 0.001 --lr_steps 30 60 --epochs 80 \
-    -b 32 -j 4 \
+    -b 32 -j 4 --dropout 0.8 \
     --snapshot_pref ucf101_bninception_ 
